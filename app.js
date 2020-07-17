abcb4bc72063dd2600bb573cfb7ecd6c160e74a6
Philip Yap <philipyap@Philips-MacBook.local>
2020-07-17 14:10:32 -0700

    [muscle-memory]1

---
+++ app.js
@@ -0,0 +1,21 @@
+const name = ['I am Philip'];
+console.log(name);
+
+
+var num = [18];
+console.log(num)
+
+const cars = ['honda', 'toyota', 'lexus'];
+console.log(cars);
+
+let number = 5
+    console.log(true);
+
+
+const things = { 
+    pet:'dog', 
+    age: 12,
+    options: ['a','b','c'], 
+    rule: 'true'
+}
+console.log(things);
